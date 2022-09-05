6193b8101fb9519e994a7dfa30cb7c6d044d4be0
igorFrotte <igorfrotte@gmail.com>
2022-09-05 15:24:37 -0300

    post messages ready

--- index.js
+++ index.js
@@ -22,6 +22,12 @@ const nameSchema = joi.object({
   name: joi.string().required()
 });
 
+const messageSchema = joi.object({
+  to: joi.string().required(),
+  text: joi.string().required(),
+  type: joi.string().required().valid('message','private_message')
+});
+
 app.post("/participants", async (req, res) => {
   const { name } = req.body;
 
@@ -69,6 +75,44 @@ app.get("/participants", async (req, res) => {
     res.send(response);
   } catch (error) {
     res.sendStatus(500).send(error.message);
+  }
+});
+
+app.post("/messages", async (req, res) => {
+  const { to, text, type } = req.body;
+  const { user } = req.headers;
+
+  const validation = messageSchema.validate(req.body, { abortEarly: false });
+
+  try {
+    const userExist = await db
+      .collection("participante")
+      .findOne({ name: user });
+    
+    if(!userExist){
+      res.status(422).send("Usuário não existe");
+      return;
+    }
+
+    if(validation.error) {
+      const erros = validation.error.details.map((detail) => detail.message);
+      res.status(422).send(erros);
+      return;
+    }
+    
+    await db
+      .collection("mensagem")
+      .insertOne({
+        from: user, 
+        to, 
+        text, 
+        type, 
+        time: dayjs(new Date()).format('HH:mm:ss')
+      });
+
+    res.status(201).send(); 
+  } catch (error) {
+    res.status(500).send(error.message);
   }
 });
 
